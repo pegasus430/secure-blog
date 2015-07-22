b0e840ed292904f8fb0252fdbf0b49c8615b1b37
softcomet <chriscomet210@gmail.com>
2015-07-22 17:01:37 -0400

    A little change...

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   
-  root 'static_pages/home'
+  root to: 'static_pages#home'
+
   get 'static_pages/help'
-  get 'static_pages/help'
+  get 'static_pages/about'
 
 end
