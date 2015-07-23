1fe1fe5cb7236292ebcfa46c84130becac2171c6
softcomet <chriscomet210@gmail.com>
2015-07-23 11:54:28 -0400

    User model is created...

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,6 @@ Rails.application.routes.draw do
   get 'help'    => 'static_pages#help'
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
-  get 'signup' => 'users/new'
+  get 'signup'  => 'users#new'
   
 end
