2481c7fc6cfad8eb5eaf4f2444bb432a607ab7f0
softcomet <chriscomet210@gmail.com>
2015-07-23 11:08:09 -0400

    Used the named routes...

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   
-  root to: 'static_pages#home'
+  # root to: 'static_pages#home'
 
-  get 'static_pages/help'
-  get 'static_pages/about'
+  # get 'static_pages/help'
+  # get 'static_pages/about'
+  # get 'static_pages/contact'
 
+  root             'static_pages#home'
+  get 'help'    => 'static_pages#help'
+  get 'about'   => 'static_pages#about'
+  get 'contact' => 'static_pages#contact'
+  
 end
