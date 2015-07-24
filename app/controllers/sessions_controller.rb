35e3c402033e112c1333cbd339cddcd3256e554b
softcomet <chriscomet210@gmail.com>
2015-07-24 11:22:12 -0400

    Logging out is implemented.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # Log the user in and redirect to the user's show page.
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
@@ -14,5 +15,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    log_out
+    redirect_to root_url
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,28 @@
 class UsersController < ApplicationController
-  def new
-    @user = User.new
-  end
+
   def show
     @user = User.find(params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
-      #redirect_to user_url(@user)
     else
       render 'new'
     end
   end
-  def user_params
-    params.require(:user).permit(:name, :email, :password,
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
-  end
+    end
 end
