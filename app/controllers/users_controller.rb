2082a8740b3b4a181315899301aca2cafe7f9e7e
softcomet <chriscomet210@gmail.com>
2015-07-24 08:55:19 -0400

    Validation result will be displayed...

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,16 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+  def create
+    @user = User.new(user_params)    # Not the final implementation!
+    if @user.save
+      # Handle a successful save.
+    else
+      render 'new'
+    end
+  end
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+  end
 end
